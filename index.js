988a4cc9dc36d00690184aa4cb11e9adbd5f6566
charliedowler <charliedowler15@gmail.com>
2014-08-24 13:20:09 +0100

    Added is-mysql

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict';
 var isMongo = require('is-mongo');
+var isMysql = require('is-mysql');
 var Q = require('q');
 module.exports = function () {
-  var promises = [isMongo()];
+  var promises = [isMongo(), isMysql()];
   return Q.all(promises);
 };
