7dfa64599c8b2d7a013bb2e36bdef0be47b56eb3
charliedowler <charliedowler15@gmail.com>
2014-09-01 21:30:16 +0100

    Included all modules

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 'use strict';
 var isMongo = require('is-mongo');
 var isMysql = require('is-mysql');
+var isCouch = require('is-couch');
+var isRedis = require('is-redis');
+var isPostgres = require('is-postgres');
 var Q = require('q');
 module.exports = function () {
-  var promises = [isMongo(), isMysql()];
+  var promises = [isMongo(), isMysql(), isCouch(), isRedis(), isPostgres()];
   return Q.all(promises);
 };
